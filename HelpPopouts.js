dca8fa8b8ec22c5a3398503ba15f5ee864fb6cc5
joseph <joseph_chapman@hotmail.com>
2011-11-07 12:39:25 +0000

    removed namespace function

--- HelpPopouts.js
+++ HelpPopouts.js
@@ -1,19 +1,5 @@
-"use strict";
-var Creatify = Creatify || {};
 
-Creatify.namespace = function(ns, container) {
-    var parts = ns.split('.'), i, len, parent = this;
-    if (parts[0] === container) {
-        parts.shift();
-    }
-    for (i = 0, len = parts.length; i < len; i++) {
-        if (typeof parent[parts[i]] === 'undefined') {
-            parent[parts[i]] = {};
-        }
-        parent = parent[parts[i]];   
-    }    
-    return parent;
-};
+"use strict";
 
 Creatify.namespace('Creatify.dom');
 
