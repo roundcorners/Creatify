1de853966a17a79f3ceae3bcf3bbabf055a19e16
Joseph Chapman <joseph.chapman@inline-block.co.uk>
2011-11-07 12:57:26 +0000

    removed test: parts[0] === this

--- namespace.js
+++ namespace.js
@@ -2,7 +2,7 @@ var Creatify = Creatify || {};
 
 Creatify.namespace = function(ns, container) {
     var parts = ns.split('.'), i, len, parent = this;
-    if (parts[0] === container || parent) {
+    if (parts[0] === container) {
         parts.shift();
     }
     for (i = 0, len = parts.length; i < len; i++) {
