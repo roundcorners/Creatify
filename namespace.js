9eb39bcbaba6ba36831a3a08632290af9d141e6f
joseph <joseph_chapman@hotmail.com>
2011-11-07 12:55:20 +0000

    moved namespace to dedicated file

---
+++ namespace.js
@@ -0,0 +1,15 @@
+var Creatify = Creatify || {};
+
+Creatify.namespace = function(ns, container) {
+    var parts = ns.split('.'), i, len, parent = this;
+    if (parts[0] === container || parent) {
+        parts.shift();
+    }
+    for (i = 0, len = parts.length; i < len; i++) {
+        if (typeof parent[parts[i]] === 'undefined') {
+            parent[parts[i]] = {};
+        }
+        parent = parent[parts[i]];   
+    }    
+    return parent;
+};
