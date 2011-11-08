1430e996fd399119c1318e276d185e2c5bc6b694
joseph <joseph_chapman@hotmail.com>
2011-11-08 11:14:08 +0000

    added observer.js

--- utils/namespace.js
+++ utils/namespace.js
@@ -1,15 +1,15 @@
 var Creatify = Creatify || {};
 
+// Namespace function
 Creatify.namespace = function(ns, container) {
-    var parts = ns.split('.'), i, len, parent = this;
+    var parts = ns.split('.'), part, i, len, parent = this;
     if (parts[0] === container) {
-        parts.shift();
+        parts.slice(0,1);
     }
     for (i = 0, len = parts.length; i < len; i++) {
         if (typeof parent[parts[i]] === 'undefined') {
             parent[parts[i]] = {};
         }
-        parent = parent[parts[i]];   
-    }    
+        parent = parent[parts[i]];   }    
     return parent;
 };
